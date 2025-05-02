8022d3589950a7fd64c31693f674fe7fe233d0a9
Deyson Venté <deiandresvent@hotmail.com>
2025-05-01 20:01:28 -0500

    App funcionando. Se muestra el listado de videos devuelto por el backend

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 using BlazorApp_WASM_WebScrapping_Youtube;
+using BlazorApp_WASM_WebScrapping_Youtube.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<YoutubeScrappingService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7045") });
 
 await builder.Build().RunAsync();
---
+++ Services/YoutubeScrappingService.cs
@@ -0,0 +1,39 @@
+﻿using BlazorApp_WASM_WebScrapping_Youtube.Models;
+using System.Net.Http.Json;
+
+namespace BlazorApp_WASM_WebScrapping_Youtube.Services
+{
+    public class YoutubeScrappingService(HttpClient http)
+    {
+        private readonly HttpClient _httpClient = http;
+
+        public async Task<List<Video>> GetVideos()
+        {
+            try
+            {
+                var req = await _httpClient.GetFromJsonAsync<List<Video>>("/api/Videos");
+                return req ?? [];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al obtener videos: {ex.Message}");
+                return [];
+                throw;
+            }
+        }
+        public async Task<List<Video>> GetPupularVideos()
+        {
+            try
+            {
+                var req = await _httpClient.GetFromJsonAsync<List<Video>>("/api/Videos/PopularVideos");
+                return req ?? [];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al obtener videos: {ex.Message}");
+                return [];
+                throw;
+            }
+        }
+    }
+}
